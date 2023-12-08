54830c472776251dcd84f805137ba0ab34faafb9
Félix Bonaert <fbonaert@Felixs-MacBook-Air.local>
2023-12-08 16:07:34 +0100

    Defined LENGTH and WIDTH in raycasting.h

--- Sources/main.c
+++ Sources/main.c
@@ -5,7 +5,6 @@ int	main(int argc, char **argv)
 	t_map		*map;
 
 	map = NULL;
-	window = NULL;
 	if (!arg_checker(argc, argv))
 		return (ft_putstr_fd("Error\nFile extension is not '.cub'\n", 2));
 	if (!map_checker(argv, map))
