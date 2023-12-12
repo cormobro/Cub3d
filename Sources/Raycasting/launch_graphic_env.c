135a07b19d893b1ec6c8ed10711b3e068d8e3f23
Félix Bonaert <fbonaert@Felixs-MacBook-Air.local>
2023-12-12 15:30:14 +0100

    quick fix

--- Sources/Raycasting/launch_graphic_env.c
+++ Sources/Raycasting/launch_graphic_env.c
@@ -14,6 +14,8 @@ void	launch_graphic_env(t_map *map)
 	window.img = mlx_new_image(window.mlx, WIDTH, LENGTH);
 	window.addr = mlx_get_data_addr(window.img, &window.bits_per_pixel, &window.line_length, &window.endian);
 	window.img = mlx_xpm_file_to_image(window.mlx, map->north, &window.line_length, &window.endian);
+	window.line_length = 160;
+	window.endian = 160;
 	mlx_put_image_to_window(window.mlx, window.mlx_win, window.img, 0, 0);
 	mlx_hook(window.mlx_win, 17, 0, ft_closebis, &window);
 	mlx_key_hook(window.mlx_win, ft_close, &window);
